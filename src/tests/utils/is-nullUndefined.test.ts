725cb0ba06ca4f03db2cda456cfd9bf37e1ea3c7
Alex Canales <alexcanales766@gmail.com>
2023-04-30 15:17:21 -0400

    Created is undefined

--- src/tests/utils/is-nullUndefined.test.ts
+++ src/tests/utils/is-nullUndefined.test.ts
@@ -1,4 +1,4 @@
-import { isNullUndefined } from "../../utils/is-nullUndefined";
+import { isNullUndefined, isUndefined } from "../../utils/is-nullUndefined";
 
 describe('isNullUndefined', () => {
     it('should return true if value is null or undefined', () => {
@@ -13,6 +13,23 @@ describe('isNullUndefined', () => {
         expect(isNullUndefined(false)).toBe(false);
         expect(isNullUndefined({})).toBe(false);
         expect(isNullUndefined([])).toBe(false);
+    });
+    }
+);
+
+describe('isUndefined', () => {
+    it('should return true if value is undefined', () => {
+        expect(isUndefined(undefined)).toBe(true);
+    });
+    
+    it('should return false if value is not undefined', () => {
+        expect(isUndefined('')).toBe(false);
+        expect(isUndefined(0)).toBe(false);
+        expect(isUndefined(true)).toBe(false);
+        expect(isUndefined(false)).toBe(false);
+        expect(isUndefined({})).toBe(false);
+        expect(isUndefined([])).toBe(false);
+        expect(isUndefined(null)).toBe(false);
     });
     }
 );
--- src/utils/is-nullUndefined.ts
+++ src/utils/is-nullUndefined.ts
@@ -1,3 +1,7 @@
 export const isNullUndefined = (value: unknown): value is null | undefined => {
     return value === null || value === undefined;
 }
+
+export const isUndefined = (value: unknown): value is undefined => {
+    return value === undefined;
+}
