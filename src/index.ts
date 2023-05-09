5f3a905799571f5fe7684bfd13d4bf43f9ca03b2
Alex Canales <alexcanales766@gmail.com>
2023-05-09 01:03:51 -0400

    Added cors example

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,18 @@ import { logger } from './middleware/index';
 import routes from './routes';
 import helmet from 'helmet';
 import { rateLimitMiddleware } from './middleware/rate-limit-middleware';
+import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const corsOptions = {
+  origin: ['http://127.0.0.1:8080', 'https://example2.com']
+};
+
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
