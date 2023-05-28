6a02b39acdd72dc8f87fb5915b68b8bc7f492a26
Alex Canales <alexcanales766@gmail.com>
2023-05-28 07:11:25 -0400

    Speed abd traffic swagger

--- src/routes/speed-camera-routes.ts
+++ src/routes/speed-camera-routes.ts
@@ -2,7 +2,66 @@ import { Router } from 'express';
 import { getSpeedCameraController, getSpeedCamerasController } from '../controllers/speed-camera-controller';
 
 const router = Router();
+/**
+ * @swagger
+ * /speed-camera/{id}:
+ *   get:
+ *     summary: Retrieve a speed camera by its id
+ *     tags: [Speed Cameras]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: Unique id of the speed camera
+ *       - in: query
+ *         name: format
+ *         schema:
+ *           type: string
+ *           enum: [geojson]
+ *         description: Format of the returned data
+ *     responses:
+ *       200:
+ *         description: The speed camera data
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/TrafficCamera'
+ *       404:
+ *         description: The speed camera was not found
+ *       500:
+ *         description: There was some server error
+ */
 router.get('/speed-camera/:id', getSpeedCameraController);
+
+/**
+ * @swagger
+ * /speed-cameras:
+ *   get:
+ *     summary: Retrieve all speed cameras
+ *     tags: [Speed Cameras]
+ *     parameters:
+ *       - in: query
+ *         name: format
+ *         schema:
+ *           type: string
+ *           enum: [geojson]
+ *         description: Format of the returned data
+ *     responses:
+ *       200:
+ *         description: The list of the speed cameras
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/TrafficCamera'
+ *       404:
+ *         description: No speed cameras were found
+ *       500:
+ *         description: There was some server error
+ */
 router.get('/speed-cameras', getSpeedCamerasController);
 
 export default router;
--- src/routes/traffic-camera-route.ts
+++ src/routes/traffic-camera-route.ts
@@ -2,11 +2,125 @@ import { Router } from 'express';
 import { getTrafficCamera, getTrafficCameras, createTrafficCamera, updateTrafficCamera, deleteTrafficCamera, deleteTrafficCameras } from '../controllers/traffic-camera-controller';
 
 const router = Router();
+
+/**
+ * @swagger
+ * /traffic-camera/{id}:
+ *   get:
+ *     summary: Retrieve a specific traffic camera
+ *     tags: [Traffic Camera]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The traffic camera ID
+ *     responses:
+ *       200:
+ *         description: A single traffic camera object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/TrafficCamera'
+ *       404:
+ *         description: Traffic camera not found
+ *       500:
+ *         description: Server error
+ */
 router.get('/traffic-camera/:id', getTrafficCamera);
+
+/**
+ * @swagger
+ * /traffic-cameras:
+ *   get:
+ *     summary: Retrieve all traffic cameras
+ *     tags: [Traffic Camera]
+ *     responses:
+ *       200:
+ *         description: An array of traffic camera objects
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/TrafficCamera'
+ *       500:
+ *         description: Server error
+ */
 router.get('/traffic-cameras', getTrafficCameras);
+
+// As the implementation of other methods is not complete, the responses section only contains a successful response
+// Remember to update this when you add a complete implementation
+
+/**
+ * @swagger
+ * /traffic-camera:
+ *   post:
+ *     summary: Create a traffic camera
+ *     tags: [Traffic Camera]
+ *     responses:
+ *       200:
+ *         description: Traffic camera created
+ *       500:
+ *         description: Server error
+ */
 router.post('/traffic-camera', createTrafficCamera);
+
+/**
+ * @swagger
+ * /traffic-camera/{id}:
+ *   put:
+ *     summary: Update a specific traffic camera
+ *     tags: [Traffic Camera]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The traffic camera ID
+ *     responses:
+ *       200:
+ *         description: Traffic camera updated
+ *       500:
+ *         description: Server error
+ */
 router.put('/traffic-camera/:id', updateTrafficCamera);
+
+/**
+ * @swagger
+ * /traffic-camera/{id}:
+ *   delete:
+ *     summary: Delete a specific traffic camera
+ *     tags: [Traffic Camera]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The traffic camera ID
+ *     responses:
+ *       200:
+ *         description: Traffic camera deleted
+ *       500:
+ *         description: Server error
+ */
 router.delete('/traffic-camera/:id', deleteTrafficCamera);
+
+/**
+ * @swagger
+ * /traffic-cameras:
+ *   delete:
+ *     summary: Delete all traffic cameras
+ *     tags: [Traffic Camera]
+ *     responses:
+ *       200:
+ *         description: All traffic cameras deleted
+ *       500:
+ *         description: Server error
+ */
 router.delete('/traffic-cameras', deleteTrafficCameras);
 
 export default router;
